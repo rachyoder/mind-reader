8a388158e3e93fdfe07ad4ded964e34dcf8c0e49
Rachael Yoder <wayoder4@gmail.com>
2019-09-13 19:17:58 -0400

    randomized results

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
 var currentState = 0;
 var states = ["home", "pick num", "add", "subtract", "symbol", "end"];
+var symbols = ["!", "@", "#", "$", "%", "*", "&"];
+var numbers = [];
+var previousSymbol
 
 var bigText = document.querySelector(".bigText");
 var nextBtn = document.getElementById("nextBtn");
 var smallText = document.querySelector(".smallText");
 var resetBtn = document.getElementById("resetBtn");
-console.log(states[currentState]);
-resetBtn.addEventListener("onclick", resetState());
-nextBtn.addEventListener("onclick", changeState());
+
+resetBtn.addEventListener("click", resetState);
+nextBtn.addEventListener("click", changeState);
+
+function randomInt(int) {
+    return Math.floor(Math.random() * Math.floor(int));
+}
 
 function resetState() {
     currentState = 0;
@@ -18,7 +25,7 @@ function changeState() {
     currentState++;
     renderState();
 }
-console.log(states[currentState]);
+
 function renderState() {
     
     switch (states[currentState]) {
@@ -26,13 +33,17 @@ function renderState() {
         case "home": 
             bigText.textContent = "I can read your mind";
             nextBtn.style.visibility = 'visible';
-            //nextBtn.textContent = "Next";
+            resetBtn.style.visibility = 'hidden';
+            smallText.style.visibility = 'hidden';
+            nextBtn.textContent = "Next";
             resetBtn.textContent = "GO";
             break;
         
         case "pick num": 
             bigText.textContent = "Pick a number between 01 - 99";
             nextBtn.style.visibility = 'visible';
+            smallText.style.visibility = 'visible';
+            resetBtn.style.visibility = 'visible';
             nextBtn.textContent = "Next";
             smallText.textContent = "When you have picked your number hit \'Next\'";
             resetBtn.textContent = "Reset";
@@ -41,31 +52,56 @@ function renderState() {
         case "add": 
             bigText.textContent = "Add both digits together to get a new number";
             nextBtn.style.visibility = 'visible';
+            smallText.style.visibility = 'visible';
+            resetBtn.style.visibility = 'visible';
             nextBtn.textContent = "Next";
-            smallText.textContent = "Ex: 42 is 4 + 2 = 6 <br>Ex: 06 is 0 + 6 = 6";
+            smallText.textContent = "Ex: 42 is 4 + 2 = 6 \nEx: 06 is 0 + 6 = 6";
             resetBtn.textContent = "Reset";
             break;
         
         case "subtract": 
             bigText.textContent = "Subtract your new number from the original number";
             nextBtn.style.visibility = 'visible';
+            smallText.style.visibility = 'visible';
+            resetBtn.style.visibility = 'visible';
             nextBtn.textContent = "Next";
-            smallText.textContent = "Ex: 42 - 6 = 36 <br>Ex: 06 - 6 = 0";
+            smallText.textContent = "Ex: 42 - 6 = 36 \nEx: 06 - 6 = 0";
             resetBtn.textContent = "Reset";
             break;
         
         case "symbol": 
-            bigText.textContent = "Filler Content";
+            bigText.innerHTML = "";
+            for (i = 0; i < 100; i++) {
+                if (i % 9 == 0) {
+                    numSymbol = i + " - " + symbols[symbols.length - 1] + "<br>";
+                    numbers.push(numSymbol);
+                    bigText.innerHTML += numSymbol;
+                } else {
+                    numSymbol = i + " - " + symbols[randomInt(6)] + "<br>";
+                    numbers.push(numSymbol);
+                    bigText.innerHTML += numSymbol;
+                }
+            }
             nextBtn.style.visibility = 'visible';
+            smallText.style.visibility = 'visible';
+            resetBtn.style.visibility = 'visible';
             nextBtn.textContent = "Reveal";
-            smallText.textContent = "Find your new number<br>Take note of the symbol beside your number";
+            smallText.textContent = "Find your new number \nTake note of the symbol beside your number";
             resetBtn.textContent = "Reset";
-        
+            break;
         case "end": 
-            bigText.textContent = "Your Symbol Goes here";
+            do {
+                var latestSymbol = symbols[randomInt(7)];
+            } while(latestSymbol == previousSymbol);
+            console.log(latestSymbol);
+            previousSymbol = latestSymbol;
+            bigText.textContent = latestSymbol;
             nextBtn.style.visibility = 'hidden';
-            smallText.textContent = "Your symbol is: "
+            smallText.style.visibility = 'visible';
+            resetBtn.style.visibility = 'visible';
+            smallText.textContent = "Your symbols is: " + latestSymbol;
             resetBtn.textContent = "Reset";
-        
+            break;
     }
 }
+renderState();
